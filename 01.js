d3ef48624dc2be7a40e4bf45c5597967873463f6
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-05-10 02:17:42 +0900

    JS 실습 01

---
+++ 01.js
@@ -0,0 +1,16 @@
+// 1.
+const me = {
+  name: "최아름",
+  age: 23,
+  hobbies: ["Playing Bass👩‍🎤", "baking👩‍🍳"],
+  likes: ["Cats🐈", "Eating🍽️"],
+};
+
+console.log(me);
+
+// 2.
+let mathScore = "77";
+let engScore = "88";
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+
+console.log(avgScore); //82.5
