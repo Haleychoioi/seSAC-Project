5a74a699fd3322ec5aa259bb1be541b4270b8b18
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-05-10 02:35:05 +0900

    JS 실습 02

---
+++ 02.js
@@ -0,0 +1,16 @@
+// 1.
+function multifly(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multifly(3, 7)); // 21
+console.log(multifly(2, 2)); // 4
+
+// 2.
+const square = (num) => {
+  console.log(num ** 2);
+};
+
+square(4); // 16
+square(11); // 121
+square(6); // 36
