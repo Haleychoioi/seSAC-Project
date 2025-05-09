966fc5f07401f377349660f3731f12d2ddbe850e
Haleychoioi <157025597+Haleychoioi@users.noreply.github.com>
2025-05-10 02:42:13 +0900

    JS 실습 03

---
+++ 03.js
@@ -0,0 +1,18 @@
+// 1.
+// let age = prompt("Please enter your age.");
+
+// if (age >= 20) {
+//   console.log("성인");
+// } else if (age >= 17) {
+//   console.log("고등학생");
+// } else if (age >= 14) {
+//   console.log("중학생");
+// } else if (age >= 18) {
+//   console.log("초등학생");
+// } else {
+//   console.log("유아");
+// }
+
+// 2.
+let now = new Date().getHours();
+console.log(now <= 12 ? "오전" : "오후");
